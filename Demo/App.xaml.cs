1407e2c825f270a23059a8400a45c3edbf5ae0b4
zhuzixian <zhuzixian@gmail.com>
2019-12-27 10:47:35 +0800

    changed ToggleButton style to MaterialDesignExpandCollapseToggleStyle

--- Demo/App.xaml.cs
+++ Demo/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace Demo
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// App.xaml 的交互逻辑
     /// </summary>
     public partial class App : Application
     {
